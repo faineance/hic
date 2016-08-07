64a20382bb7454f5a807869fa6b70f6f2c8901d2
faineance <faineance@users.noreply.github.com>
2016-08-07 14:26:49 +0100

    DeriveTraversable is da bomb

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,18 @@
-{-# LANGUAGE DeriveFoldable #-}
-{-# LANGUAGE DeriveFunctor  #-}
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
 module Main where
 import           Control.Applicative
 import           Data.Functor.Identity
 
-data Vec3 a = Vec3 !a !a !a deriving (Eq,Ord,Show, Functor, Foldable)
+data Vec3 a = Vec3 !a !a !a deriving (Eq,Ord,Show, Functor, Foldable, Traversable)
 
-instance Traversable Vec3 where
-  traverse f (Vec3 a b c) = Vec3 <$> f a <*> f b <*> f c
+_x, _y, _z :: Functor f => (a -> f a) -> Vec3 a -> f (Vec3 a)
+_x f (Vec3 x y z) = (\x' -> Vec3 x' y z) <$> f x
+_y f (Vec3 x y z) = (\y' -> Vec3 x y' z) <$> f y
+_z f (Vec3 x y z) = Vec3 x y <$> f z
+
+type M33 a = (Vec3 (Vec3 a))
 
 top, middle, bottom, left, center, right:: Functor f => (a -> f a) -> Vec3 a -> f (Vec3 a)
 top = _x
@@ -29,6 +34,9 @@ right = _z
 -- >>> over (traverse . _x) (\x -> Just X) initialGrid
 --
 -- >>> over (_x . traverse) (\x -> Just X) initialGrid
+--
+-- And if Cell had a monoid instance.
+-- >> view (traverse . traverse) initialGrid
 
 view ::  ((a -> Const a b) -> s -> Const a t) -> s -> a
 view l s = getConst (l Const s)
@@ -39,21 +47,20 @@ over l f = runIdentity . l (Identity . f)
 set :: ((a -> Identity b) -> (s -> Identity t)) -> b -> s -> t
 set l r = over l (const r)
 
-_x, _y, _z :: Functor f => (a -> f a) -> Vec3 a -> f (Vec3 a)
-_x f (Vec3 x y z) = (\x' -> Vec3 x' y z) <$> f x
-_y f (Vec3 x y z) = (\y' -> Vec3 x y' z) <$> f y
-_z f (Vec3 x y z) = Vec3 x y <$> f z
 
-data Cell = X | O deriving (Eq,Show)
+data Team = X | O deriving (Eq,Show)
 
-type Square = Maybe Cell
+type Square = Maybe Team
 
-type Grid a = (Vec3 (Vec3 a))
+type Grid = M33 Square
 
-initialGrid :: Grid Square
+data Game = Turn Team Grid | Won Team Grid | Draw Grid
+
+initialGrid :: Grid
 initialGrid = Vec3 emptyRow emptyRow emptyRow
     where
         emptyRow = Vec3 Nothing Nothing Nothing
+
 
 main :: IO ()
 main = putStrLn "hello world"
